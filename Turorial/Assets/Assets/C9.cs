40e2ba929983d0d6a2294ab82ae9cf3e20de47f8
CK <dearkick@163.com>
2015-11-07 17:22:34 +0800

    新建

---
+++ Turorial/Assets/Assets/C9.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class C9 : MonoBehaviour {
+
+
+	public void OnPress_Button()
+	{
+		Debug.Log ("OnPress_Button");
+
+	}
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
