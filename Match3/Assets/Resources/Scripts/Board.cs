bd9043eb525784b9c617ddc5e30951066c0e3be7
Dma <DmaInNewZealand@Gmail.com>
2015-11-11 00:01:35 +1300

    Add New Project

---
+++ Match3/Assets/Resources/Scripts/Board.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Board : MonoBehaviour
+{
+    public int gridWidth;
+    public int gridHeight;
+
+    public GameObject gemPrefab;
+
+    Gem[,] m_gems;
+    Gem m_currentGem;
+
+    void Awake()
+    {
+        m_gems = new Gem[gridWidth, gridHeight];
+    }
+
+    void Start()
+    {
+        for (int x = 0; x < gridWidth; x++)
+        {
+            for (int y = 0; y < gridHeight; y++)
+            {
+                GameObject obj = Instantiate(gemPrefab, new Vector3(-gridWidth / 2f + x + 0.5f, -gridHeight / 2f + y + 0.5f), Quaternion.identity) as GameObject;
+                obj.transform.SetParent(transform);
+                m_gems[x, y] = obj.GetComponent<Gem>();
+                m_gems[x, y].x = x;
+                m_gems[x, y].y = y;
+            }
+        }
+    }
+
+    public void SwapGems(Gem selectedGem)
+    {
+        if(m_currentGem == null)
+        {
+            m_currentGem = selectedGem;
+        }
+        else if (m_currentGem == selectedGem)
+        {
+            m_currentGem = null;
+        }
+        else if(!m_currentGem.IsAdjacent(selectedGem))
+        {
+            m_currentGem.ToggleSelector();
+            m_currentGem = selectedGem;
+        }
+        else
+        {
+            m_currentGem.ToggleSelector();
+            selectedGem.ToggleSelector();
+            m_currentGem = null;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Match3/Assets/Resources/Scripts/Data/Data.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class Data
+{
+    //--------------Neighbour---------------
+    public const int UP = 0;
+    public const int LEFT = 1;
+    public const int RIGHT = 2;
+    public const int DOWN = 3;
+
+    //--------------Gem Type---------------
+    public const int RED = 0;
+    public const int YELLOW = 1;
+    public const int BLUE = 2;
+    public const int GREEN = 3;
+    public const int PURPLE = 4;
+    public const int WHITE = 5;
+    public const int ORANGE = 6;
+
+    public static string[] GEMTYPE = new string[7] { "Red", "Yellow", "Blue", "Green", "Purple", "White", "Orange" }; 
+
+
+}
